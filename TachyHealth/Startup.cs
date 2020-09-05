031698e6a631020719d1646567a5dab564593f66
Abd El Rahman Ezzat <abdo.ezzat951@gmail.com>
2020-09-05 06:07:08 +0200

    After Add Confirmation Email

---
+++ TachyHealth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TachyHealth.Startup))]
+namespace TachyHealth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
